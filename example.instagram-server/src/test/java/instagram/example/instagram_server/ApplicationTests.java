cb0d31ca2a2bcb559052962caac2a993d069dcb8
osw <saewookdev@gmail.com>
2025-11-18 10:12:03 +0900

    2025118

---
+++ example.instagram-server/src/test/java/instagram/example/instagram_server/ApplicationTests.java
@@ -0,0 +1,13 @@
+package instagram.example.instagram_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
